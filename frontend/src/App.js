3578f58e12b234f5e36a30f9ef6884e6ad53aaec
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2020-12-21 16:44:14 -0500

    new update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Redirect,
   Route,
   Switch,
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import PlaylistsPage from "./pages/PlaylistsPage";
+import ArtistsPage from "./pages/ArtistsPage";
+import AlbumsPage from './pages/AlbumsPage';
+import SongsPage from './pages/SongsPage';
+
 
 const App = () => {
   return (
     <Router>
       <Switch>
-      <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/playlists" component={PlaylistsPage} />
+        <Route exact path="/artists" component={ArtistsPage} />
+        <Route exact path="/albums" component={AlbumsPage} />
+        <Route exact path="/songs" component={SongsPage} />
       </Switch>
     </Router>
   );
