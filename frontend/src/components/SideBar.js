16f60e034e4b2ebf65ed97d545d5d721b0ce64e2
younggg96 <yangguannggeng960123@gmail.com>
2021-03-27 01:53:06 -0400

    new update

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -3,9 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import logo from "../assets/img/heal-music-logo.png";
 import { Box, ButtonBase, Grid, List, ListItem, Typography } from "@material-ui/core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faCompactDisc, faMicrophone } from '@fortawesome/free-solid-svg-icons'
-import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
-import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import { faHome, faCompactDisc, faMicrophone, faListAlt, faMusic } from '@fortawesome/free-solid-svg-icons'
 import StoreIcon from '@material-ui/icons/Store';
 import SettingsIcon from '@material-ui/icons/Settings';
 
@@ -31,6 +29,23 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
+const sideBarArr = {
+  library: {
+    title: "library",
+    data: [
+      { title: "home", icon: faHome },
+      { title: "Playlists", icon: faCompactDisc },
+      { title: "Artists", icon: faMicrophone },
+      { title: "Albums", icon: faListAlt },
+      { title: "Songs", icon: faMusic },
+    ]
+  },
+  discover: {
+    title: "Discover",
+    data: ["Music Store", "Setting"]
+  }
+}
+
 const SideBar = () => {
   const classes = useStyles();
 
@@ -41,89 +56,29 @@ const SideBar = () => {
       </div>
       <div style={{ marginBottom: 30 }}>
         <Typography color="secondary" variant="body1" className={classes.title}>
-          <Box fontWeight="700" fontSize="18px">Library</Box>
+          <Box fontWeight="700" fontSize="18px">{sideBarArr.library.title}</Box>
         </Typography>
         <List>
-          <ListItem>
-            <ButtonBase className={classes.btn}>
-              <Grid container justify="center" alignItems="baseline" className={classes.btnContent}>
-                <Grid item xs={2}>
-                  <FontAwesomeIcon icon={faHome} size="lg" color="#ffffff" />
-                </Grid>
-                <Grid item xs>
-                  <Typography component="span" variant="body1" align="left">
-                    <Box color="text.main">
-                      Home
-                    </Box>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </ButtonBase>
-          </ListItem>
-          <ListItem>
-            <ButtonBase className={classes.btn}>
-              <Grid container justify="center" alignItems="baseline" className={classes.btnContent}>
-                <Grid item xs={2}>
-                  <FontAwesomeIcon icon={faCompactDisc} size="lg" color="#ffffff" />
-                </Grid>
-                <Grid item xs>
-                  <Typography component="span" variant="body1" align="left">
-                    <Box color="text.main">
-                      Playlists
-                    </Box>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </ButtonBase>
-          </ListItem>
-          <ListItem>
-            <ButtonBase className={classes.btn}>
-              <Grid container justify="center" alignItems="baseline" className={classes.btnContent}>
-                <Grid item xs={2}>
-                  <FontAwesomeIcon icon={faMicrophone} size="lg" color="#ffffff" />
-                </Grid>
-                <Grid item xs>
-                  <Typography component="span" variant="body1" align="left">
-                    <Box color="text.main">
-                      Artists
-                    </Box>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </ButtonBase>
-          </ListItem>
-          <ListItem>
-            <ButtonBase className={classes.btn}>
-              <Grid container justify="center" alignItems="center" className={classes.btnContent}>
-                <Grid item xs={2}>
-                  <PlaylistPlayIcon style={{ color: "#ffffff" }} />
-                </Grid>
-                <Grid item xs>
-                  <Typography component="span" variant="body1" align="left">
-                    <Box color="text.main">
-                      Albums
-                    </Box>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </ButtonBase>
-          </ListItem>
-          <ListItem>
-            <ButtonBase className={classes.btn}>
-              <Grid container justify="center" alignItems="center" className={classes.btnContent}>
-                <Grid item xs={2}>
-                  <MusicNoteIcon style={{ color: "#ffffff" }} />
-                </Grid>
-                <Grid item xs>
-                  <Typography component="span" variant="body1" align="left">
-                    <Box color="text.main">
-                      Songs
-                    </Box>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </ButtonBase>
-          </ListItem>
+          {sideBarArr.library.data.map((item, index) => {
+            return (
+              <ListItem key={index}>
+                <ButtonBase className={classes.btn}>
+                  <Grid container justify="center" alignItems="center" className={classes.btnContent}>
+                    <Grid item xs={2}>
+                      <FontAwesomeIcon icon={item.icon} size="lg" color="#ffffff" />
+                    </Grid>
+                    <Grid item xs>
+                      <Typography component="span" variant="body1" align="left">
+                        <Box color="text.main">
+                          {item.title}
+                        </Box>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </ButtonBase>
+              </ListItem>
+            )
+          })}
         </List>
       </div>
       <div>
